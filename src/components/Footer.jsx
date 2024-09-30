a8dc130761e9449f3f73cdecdf44df6788fe3700
sekullar <asdkralxasd@gmail.com>
2024-09-30 17:50:14 +0300

    Footer links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
                 <p className="opacity-50 space-grotesk-400 text-xl">Yerli malı, el emeği ile hazırlanmış <br />Linux distrosu.</p>
                 <p className="mt-3 space-grotesk-400 text-xl">Daha fazla bilgi almak için lütfen ziyaret et:</p>
                 <div className="flex items-center gap-8">
-                    <a href="#" className="font-theme-3">@ankalinuxx</a>
-                    <a href="#" className="font-theme-3">ankaos</a>
-                    <a href="#" className="space-grotesk-400 font-theme-3">(X)<span>ankalinuxx</span></a>
+                    <a href="https://www.instagram.com/ankalinuxx?igsh=bmp6dmFqamh0dGRm"target="_blank" className="font-theme-3">@ankalinuxx</a>
+                    <a href="https://youtube.com/@ankalinuxos?si=GdkcK6Ek2Ge9f8r0" target="_blank" className="font-theme-3">ankaos</a>
+                    <a href="https://x.com/ankalinuxx" target="_blank" className="space-grotesk-400 font-theme-3">(X)<span>ankalinuxx</span></a>
                 </div>
             </div>
         </>
